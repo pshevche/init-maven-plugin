36df87ec3cdac3ce685cd5baea6736a505e3a241
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2025-09-23 10:39:15 +0200

    Add required dependencies

---
+++ src/test/java/io/github/pshevche/maven/init/FooTest.java
@@ -0,0 +1,13 @@
+package io.github.pshevche.maven.init;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FooTest {
+
+    @Test
+    void successfulTest() {
+        assertTrue(true);
+    }
+}
